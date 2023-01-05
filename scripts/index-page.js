2b25bc24c78b3c1bfb2be894d619563c030be5b8
modernfaust <edward.l1245@hotmail.com>
2023-01-04 23:02:50 -0500

    define function for project details card

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,8 +1,11 @@
 let projectsInfo = document.querySelectorAll(".projects__content");
 
 projectsInfo.forEach(project => {
-    project.addEventListener ('mousedown', function() {
-        console.log('test');
+    project.addEventListener ('mousedown', function(event) {
+        event.stopPropagation();
+        let projectsDetails = document.createElement('div');
+        //this div will contain details for project
+        project.appendChild(projectsDetails);
     })
 })
 
